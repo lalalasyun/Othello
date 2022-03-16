6e1e210a6940456bc361c7e8c4d39eb04bf0d0f5
syun <lalalasyunsuke1@gmail.com>
2022-03-17 02:42:17 +0900

    ai update

--- src/main/java/model/Othello.java
+++ src/main/java/model/Othello.java
@@ -750,7 +750,7 @@ public class Othello {
 		}
 		for (int[] ary : coord) {
 			int[][] copyoth = copyOth(oth);
-			int turncnt = (64 - count(copyoth)[1] + count(copyoth)[2])/7;
+			int turncnt = (count(copyoth)[1] + count(copyoth)[2])/7;
 			int oppennes = put(ary[0], ary[1], turn ? 0 : 1, copyoth);
 			search(turn ? 1 : 0, copyoth);
 			List<int[]> getcoord = getCoord(copyoth);
